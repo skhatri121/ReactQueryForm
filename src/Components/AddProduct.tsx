e76acc15a19ee56f6201318c0fa114aadb63823b
skhatri121 <subashkhatri784@gmail.com>
2024-02-14 20:42:47 +0545

    Done

--- src/Components/AddProduct.tsx
+++ src/Components/AddProduct.tsx
@@ -1,34 +1,3 @@
-// import { useMutation, useQueryClient } from "react-query";
-// import Form from "./Form";
-// import { createProduct } from "../api/fnc";
-// import { SubmitHandler } from "react-hook-form";
-// import { v4 as uuidv4 } from "uuid";
-// const AddProduct = () => {
-//   const queryClient = useQueryClient();
-//   const createProductMutation = useMutation({
-//     mutationFn: createProduct,
-//     onSuccess: () => {
-//       queryClient.invalidateQueries({ queryKey: ["products"] });
-//       console.log("success bro!");
-//       window.location.reload();
-//     },
-//   });
-//   const handleAddProduct: SubmitHandler<FormField> = (product) => {
-//     createProductMutation.mutate({
-//       id: uuidv4(),
-//       ...product,
-//     });
-//   };
-
-//   return (
-//     <>
-//       <Form onSubmit={handleAddProduct} />
-//     </>
-//   );
-// };
-
-// export default AddProduct;
-
 // AddProduct.tsx
 import { useMutation, useQueryClient } from "react-query";
 import Form from "./Form";

57c7dfa831353a701577d09cdba62604b16676b3
skhatri121 <subashkhatri784@gmail.com>
2024-02-20 15:39:49 +0545

    Table Done

--- src/Components/CommonTable.tsx
+++ src/Components/CommonTable.tsx
@@ -1,74 +1,70 @@
-import { Button, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import {
+  Button,
+  ButtonGroup,
+  Center,
+  Stack,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 
-const CommonTable = ({
-  products,
-  selectedProductIds,
-  handleCheckboxChange,
-  navigate,
-  handleDelete,
-}) => {
+const CommonTable = ({ products, columns, navigate, handleDelete }) => {
   return (
-    <Table variant="simple">
-      <Thead>
-        <Tr>
-          <Th></Th>
-          <Th>Product Title</Th>
-          <Th>Description</Th>
-          <Th isNumeric>Price</Th>
-          <Th>Actions</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {products.map((product) => (
-          <Tr key={product.id}>
-            <Td>
-              <input
-                type="checkbox"
-                checked={selectedProductIds.includes(product.id)}
-                onChange={() => handleCheckboxChange(product.id)}
-              />
-            </Td>
-            <Td
-              cursor="pointer"
-              onClick={() => navigate(`/products/${product.id}`)}
-            >
-              {product.title}
-            </Td>
-            <Td
-              cursor="pointer"
-              onClick={() => navigate(`/products/${product.id}`)}
-            >
-              {product.description}
-            </Td>
-            <Td
-              isNumeric
-              cursor="pointer"
-              onClick={() => navigate(`/products/${product.id}`)}
-            >
-              $ {product.price}
-            </Td>
-            <Td>
-              <Button
-                onClick={() => navigate(`/products/${product.id}/edit`)}
-                mt="2"
-                bg="primary.51"
-              >
-                Edit
-              </Button>
-              <Button
-                disabled={selectedProductIds.length === 0}
-                onClick={handleDelete}
-                mt="5px"
-                bg="primary.54"
-                color="primary.59"
-              >
-                Delete
-              </Button>
-            </Td>
-          </Tr>
-        ))}
-      </Tbody>
-    </Table>
+    <Stack spacing={10}>
+      <Center>
+        <TableContainer w="1200px">
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                {columns.map((column) => (
+                  <Th key={column}>{column}</Th>
+                ))}
+                <Th>Actions</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {products.map((product) => (
+                <Tr key={product.id}>
+                  {columns.map((column) => (
+                    <Td
+                      key={`${product.id}-${column}`}
+                      cursor="pointer"
+                      onClick={() => navigate(`/products/${product.id}`)}
+                    >
+                      {product[column.toLowerCase()]}
+                    </Td>
+                  ))}
+                  <Td>
+                    <ButtonGroup gap="4">
+                      <Button
+                        colorScheme="blue"
+                        onClick={() => navigate(`/products/${product.id}/edit`)}
+                        mt="2"
+                        size="md"
+                      >
+                        Edit
+                      </Button>
+                      <Button
+                        colorScheme="blue"
+                        onClick={() => handleDelete(product.id)}
+                        mt="5px"
+                        size="md"
+                      >
+                        Delete
+                      </Button>
+                    </ButtonGroup>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Center>
+    </Stack>
   );
 };
 
--- src/Pages/ProductList.tsx
+++ src/Pages/ProductList.tsx
@@ -1,3 +1,93 @@
+// import { useState } from "react";
+// import { useMutation, useQuery, useQueryClient } from "react-query";
+// import { fetchProducts, deleteProduct } from "../api/fnc";
+// import { useNavigate } from "react-router-dom";
+// import ReactPaginate from "react-paginate";
+// import AddProduct from "../Components/AddProduct";
+// import CommonTable from "../Components/CommonTable";
+
+// const ProductList = () => {
+//   const queryClient = useQueryClient();
+//   const navigate = useNavigate();
+//   const [currentPage, setCurrentPage] = useState(0);
+//   const [selectedProductIds, setSelectedProductIds] = useState([]);
+//   const productsPerPage = 5;
+
+//   const {
+//     isLoading,
+//     isError,
+//     data: products,
+//     error,
+//   } = useQuery({
+//     queryKey: ["products"],
+//     queryFn: fetchProducts,
+//   });
+
+//   const deleteProductMutation = useMutation({
+//     mutationFn: deleteProduct,
+//     onSuccess: () => {
+//       queryClient.invalidateQueries({ queryKey: ["products"] });
+//       setSelectedProductIds([]);
+//     },
+//   });
+
+//   const handleDelete = () => {
+//     selectedProductIds.forEach((id) => {
+//       deleteProductMutation.mutate(id);
+//     });
+//   };
+
+//   const handleCheckboxChange = (productId) => {
+//     setSelectedProductIds((prevSelectedProductIds) => {
+//       if (prevSelectedProductIds.includes(productId)) {
+//         return prevSelectedProductIds.filter((id) => id !== productId);
+//       } else {
+//         return [...prevSelectedProductIds, productId];
+//       }
+//     });
+//   };
+
+//   if (isLoading) return "Loading...";
+//   if (isError) return `Error: ${error.message}`;
+
+//   if (!products || products.length === 0) {
+//     return <div>No products found.</div>;
+//   }
+
+//   const pageCount = Math.ceil(products.length / productsPerPage);
+
+//   const changePage = ({ selected }) => {
+//     setCurrentPage(selected);
+//   };
+
+//   const offset = currentPage * productsPerPage;
+
+//   return (
+//     <>
+//       <AddProduct />
+
+//       <CommonTable
+//         products={products.slice(offset, offset + productsPerPage)}
+//         selectedProductIds={selectedProductIds}
+//         handleCheckboxChange={handleCheckboxChange}
+//         navigate={navigate}
+//         handleDelete={handleDelete}
+//       />
+
+//       <ReactPaginate
+//         breakLabel={"..."}
+//         pageCount={pageCount}
+//         marginPagesDisplayed={2}
+//         pageRangeDisplayed={5}
+//         onPageChange={changePage}
+//         containerClassName={"pagination"}
+//         activeClassName={"active"}
+//       />
+//     </>
+//   );
+// };
+
+// export default ProductList;
 import { useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import { fetchProducts, deleteProduct } from "../api/fnc";
@@ -31,10 +121,8 @@ const ProductList = () => {
     },
   });
 
-  const handleDelete = () => {
-    selectedProductIds.forEach((id) => {
-      deleteProductMutation.mutate(id);
-    });
+  const handleDelete = (productId) => {
+    deleteProductMutation.mutate(productId);
   };
 
   const handleCheckboxChange = (productId) => {
@@ -62,12 +150,15 @@ const ProductList = () => {
 
   const offset = currentPage * productsPerPage;
 
+  const columns = ["Title", "Description", "Price"];
+
   return (
     <>
       <AddProduct />
 
       <CommonTable
         products={products.slice(offset, offset + productsPerPage)}
+        columns={columns}
         selectedProductIds={selectedProductIds}
         handleCheckboxChange={handleCheckboxChange}
         navigate={navigate}
